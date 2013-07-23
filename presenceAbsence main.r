c1d7810cddf6eedcd6f9f7193a29a89cf6a1f693
James-Thorson-esq <JamesT.esq@gmail.com>
2013-07-23 12:53:59 -0700

    Fixed some bugs (see below), and confirmed that slope and abundance are estimable
    Previous bugs:
    1.  Estimation and data-generating models had different treatment of
    juvenile survival, and different transformation of adult survival
    2.  Issues with having too many rep(XXX, nSites) commands, so that
    several times there'd be vectors of length nSites^2
    
    Other changes:
    A.  Added parameter labels where possible
    B.  Added function to only optimize 1-2 parameters (slope + abundance)

--- presenceAbsence main.r
+++ presenceAbsence main.r
@@ -4,6 +4,7 @@ library(corpcor)
 library(mcmc)
 library(IMIS)
 library(mvtnorm)
+library(pso)
 
 # set wd
 setwd("/users/eric.ward/documents/projects/presenceAbsence")
@@ -13,46 +14,71 @@ setwd("/users/eric.ward/documents/projects/presenceAbsence")
 source("presenceAbsence functions.r")
 sourceCpp("code.cpp", verbose=TRUE)
 
-observedStages = 2 # juvenile and adult PA data observed, otherwise = 1
+observedStages = 1 # juvenile and adult PA data observed, otherwise = 1
 
 ####################################################################
 # demonstrate how to simulate data
 ####################################################################
 nSite=50
-beta1 = 0.8
-dat = randPA(nSites=nSite, lambda=1.5, sigma=1, B1=beta1, locs = NULL, distMat = NULL, temp=NULL, fecund=0.9, survJ = 0.1, survA = 0.2, migRate = 0.05, N = 100)
+beta1 = 0.6
+  # nSites=nSite; lambda=1.5; sigma=1; B1=beta1; locs = NULL; distMat = NULL; temp=NULL; fecund=0.9; survJ = 0.5; survA = 0.2; migRate = 0.05; N = 100
+dat = randPA(nSites=nSite, lambda=1.5, sigma=1, B1=beta1, locs = NULL, distMat = NULL, temp=NULL, fecund=0.9, survJ = 0.5, survA = 0.2, migRate = 0.05, N = 100)
 temp=dat$temp
 pdf("Survival transformations.pdf")
-# test the presence - absence function
-par(mfrow=c(3,2), mai = c(0.6,0.6,0.1,0.1))
-plot(seq(1,length(dat$temp)),dat$temp, xlab ="Temp", ylab="Uniform(0,1)",type="b",lwd=2)
-legend('topleft',("1"))
-plot(dat$temp, dat$logitA, xlab = "Temp", ylab = "Survival - logit space",type="b",lwd=2)
-legend('topleft',("2"))
-plot(dat$temp, dat$survA, xlab = "Temp", ylab = "Survival - normal space",type="b",lwd=2,ylim=c(0,1))
-legend('topleft',("3"))
-plot(dat$temp, dat$probs[seq(2,length(dat$PA),2)], xlab = "Temp", ylab = "Survival - postEigen")
-legend('topleft',("4"))
-plot(dat$temp,dat$PA[seq(2,length(dat$PA),2)], xlab="Temp",ylab="Data")
-legend('topleft',("5"))
+  # test the presence - absence function
+  par(mfrow=c(3,2), mai = c(0.6,0.6,0.1,0.1))
+  plot(seq(1,length(dat$temp)),dat$temp, xlab ="Temp", ylab="Uniform(0,1)",type="b",lwd=2)
+  legend('topleft',("1"))
+  plot(dat$temp, dat$logitA, xlab = "Temp", ylab = "Survival - logit space",type="b",lwd=2)
+  legend('topleft',("2"))
+  plot(dat$temp, dat$survA, xlab = "Temp", ylab = "Survival - normal space",type="b",lwd=2,ylim=c(0,1))
+  legend('topleft',("3"))
+  plot(dat$temp, dat$probs[seq(2,length(dat$PA),2)], xlab = "Temp", ylab = "Survival - postEigen")
+  legend('topleft',("4"))
+  plot(dat$temp,dat$PA[seq(2,length(dat$PA),2)], xlab="Temp",ylab="Data")
+  legend('topleft',("5"))
 dev.off()
 
 ####################################################################
 # demonstrate how to find MLEs with optim
 ####################################################################
-nSite = 50
+nSite = 200
+  # Inputs for walking through the function randPA: nSites=nSite; lambda=1.5; sigma=1; B1=beta1; locs = NULL; distMat = NULL; temp=NULL; fecund=0.9; survJ = 0.5; survA = 0.2; migRate = 0.05; N = 100
+dat = randPA(nSites=nSite, lambda=1.5, sigma=1, B1=3, locs = NULL, distMat = NULL, temp=NULL, fecund=0.9, survJ = qlogis(0.5), survA = qlogis(0.2), migRate = 0.05, N = 100)
+y = dat$PA                                                                                          # survJ -> Logit-space
+temp = dat$temp                                                                                     # survA -> Logit-space
+TruePars = par = c(log(0.9), qlogis(0.5), qlogis(0.2), qlogis(0.05), log(100), 3) 
 
-dat = randPA(nSites=50, lambda=1.5, sigma=1, B1=5, locs = NULL, distMat = NULL, temp=NULL, fecund=0.9, survJ = 0.1, survA = 0.2, migRate = 0.05, N = 100)
-y = dat$PA
-temp = dat$temp
-mlEst = optim(runif(6), estimateML)
-backCalculateParameters(mlEst$par)
+# Try estimating all parameters
+# CONCLUSION: NEVER WORKS WELL WITH JUST PRESENCE-ABSENCE DATA
+if(FALSE){
+  mlEst = optim(runif(6), estimateML)    # Parameters: 1=log-Fecundity; 2=logit-junvenile survival; 3=logit-adult survival intercept; 4=logit-migration rate; 5=log-N; 6=survival slope
+  backCalculateParameters(mlEst$par)
+}
+
+# Try estimating only the environmental parameter
+# CONCLUSION: CAN WORK, BUT IS POSITIVELY BIASED (AS IS CONSISTENT WITH INFREQUENT-EVENT LOGISTIC REGRESSION)
+Fixed = TruePars
+  Fixed[6] = NA
+( mlEst = optimize(f=FixedFn, Fixed=c(log(0.9), qlogis(0.5), qlogis(0.2), qlogis(0.05), log(100), NA), interval=c(-10,10)) )    
+  Est = Fixed
+  Est[6] = mlEst$minimum
+backCalculateParameters(Est)
+
+# Try estimating the environmental parameter + Abundance
+# CONCLUSION: POSITIVE BIAS IN ENVIRONMENTAL EFFECT TRANSLATES TO NEGATIVE BIAS IN N
+Fixed = TruePars
+  Fixed[5:6] = NA
+( mlEst = nlminb(start=runif(2), objective=FixedFn, Fixed=Fixed, control=list(trace=1)) )    
+  Est = Fixed
+  Est[5:6] = mlEst$par
+backCalculateParameters(Est)
 
 ####################################################################
 # demonstrate how to find posteriors with ISIS
 ####################################################################
 nSite = 50
-dat = randPA(nSites=50, lambda=1.5, sigma=1, B1=5, locs = NULL, distMat = NULL, temp=NULL, fecund=0.9, survJ = 0.1, survA = 0.2, migRate = 0.05, N = 100)
+dat = randPA(nSites=nSite, lambda=1.5, sigma=1, B1=5, locs = NULL, distMat = NULL, temp=NULL, fecund=0.9, survJ = 0.1, survA = 0.2, migRate = 0.05, N = 100)
 y = dat$PA
 temp = dat$temp
 o = IMIS(B=500, B.re=500, number_k=100, D=10)
