46708c178ba3d5f080dbd3465a9fdc3f4d50f9b1
eric-ward <eric.ward@noaa.gov>
2013-08-20 13:21:55 -0700

    updates

--- presenceAbsence_realData.R
+++ presenceAbsence_realData.R
@@ -16,35 +16,30 @@ sourceCpp("code.cpp", verbose=TRUE)
 
 observedStages = 2 # juvenile and adult PA data observed, otherwise = 1
 
-# load in data
-load("/Users/Eric.Ward/Downloads/ONP_SoleDuc98_amphibs.RData")
+# For Ben: you'll need to create a vector 'y' of 0s and 1s that alternates between stage, then across sites. And a vector of covariates named 'temp'
+nSite = 100
+y = c(sample(c(0,1), size=200, replace=T))
+temp = rnorm(nSite,0,1) # the covariate needs to be named 'temp'
 
-# For Ben: you'll need to create a vector 'y' of 0s and 1s that alternates between stage, then across sites. For example 
+# Fix the demographic rates 
+fecundity = 0.9
+juvSurvival = 0.8
+adultSurvival = 0.99
+migrationRate = 0.05
 
-# get data for amma
-amma = dat[,c("AMMA","Northing","Easting","elev")]
-# create distance matrix
-amma.dist = dist(amma[,c("Northing","Easting")], diag=T, upper=T)
-# convert AMMA data to 0s and 1s based on juvenile (2)/ adult (1)
-# format is Site1-stage1, Site1-stage2, Site2-stage1, Site2-stage2
-nSite=dim(amma)[1]
-y = rep(0, nSite*2)
-y[which(amma$AMMA==2)*2-1] = 1 # fill in juveniles
-y[which(amma$AMMA==1)*2] = 1 # fill in adults
-
-temp = log(amma$elev) # the covariate needs to be named 'temp'
 # Parameters: 1=log-Fecundity; 2=logit-junvenile survival; 3=logit-adult survival intercept; 4=logit-migration rate; 5=log-N; 6=survival slope
 temp = scale(temp) # ~ N(0,1)
-# 1. FIT THE MAXIMUM LIKELIHOOD MODEL HERE
-Fixed = c(log(0.9), qlogis(0.8), qlogis(0.99), qlogis(0.05), NA, NA) 
-mlEst = nlminb(start=runif(2), objective=FixedFn, Fixed=Fixed, control=list(trace=1))     
-Est = Fixed
-Est[5:6] = mlEst$par
-backCalculateParameters(Est)
+
+# UNCOMMENT TO FIT THE MAXIMUM LIKELIHOOD MODEL HERE
+#Fixed = c(log(fecundity), qlogis(juvSurvival), qlogis(adultSurvival), qlogis(migrationRate), NA, NA) 
+#mlEst = nlminb(start=runif(2), objective=FixedFn, Fixed=Fixed, control=list(trace=1))     
+#Est = Fixed
+#Est[5:6] = mlEst$par
+#backCalculateParameters(Est)
 
 # This is just another version of the function in "presenceAbsence functions.r" -- the difference is here, the first 4 parameters are set
 calcLike = function(par) {
-  pop = calcPop(c(log(0.9), qlogis(0.8), qlogis(0.8),qlogis(0.05), par[5], par[6])) # calculate s.a.d.
+  pop = calcPop(c(log(fecundity), qlogis(juvSurvival), qlogis(adultSurvival), qlogis(migrationRate), par[5], par[6])) # calculate s.a.d.
   N = exp(par[5]) # this is local variable
   # calculate site-stage probabilities
   nSite2 = nSite*2
@@ -57,26 +52,27 @@ calcLike = function(par) {
   return(L)
 }
 
-# Estimate the Bayesian model here with SIR model
+# Estimate the Bayesian model here with SIR model. You should be able to let it run as is -- but you might want to adjust the maximum possible population size 'maxN' below -- for the ONP data, we used log(600). 
 nDraws = 1000
 iter = 0
 cumLike = 0
-thresh = 4.0e-28
+thresh = 0
+maxN = 600
 savedDraws = matrix(0,nDraws,2)
 while(iter < nDraws) {
 	# draw from priors on log(N) and B.env
-	logN = runif(1, 0, log(600))
+	logN = runif(1, 0, log(maxN))
 	B.env = runif(1, -5, 5)
 	
 	# calculate likelihood
-	like = exp(calcLike(c(log(0.9), qlogis(0.8), qlogis(0.8),qlogis(0.05), logN, B.env)))
+	like = exp(calcLike(c(log(fecundity), qlogis(juvSurvival), qlogis(adultSurvival), qlogis(migrationRate), logN, B.env)))
 	if(is.na(like)==F) {
 	  cumLike = cumLike + like
-	  if(like> best) { # this if statement makes the algorithm reset if a better value is found
+	  if(like> thresh) { # this if statement makes the algorithm reset if a better value is found
  	  	iter = 0
 	  	cumLike = 0
 	  	savedDraws = savedDraws*0
-	  	best = like
+	  	thresh = like
 	  }
 	  if(cumLike > thresh) {
 		while(cumLike > thresh) {
@@ -92,5 +88,5 @@ while(iter < nDraws) {
 }
 
 par(mfrow=c(2,1), mai = c(1,1,0.2,0.2))
-hist(savedDraws[,1], 40, xlab = "Log (N)", ylab="Posterior", col="grey",main="AMMA ONP")
-hist(savedDraws[,2], 40, xlab = "B.elevation", ylab="Posterior", col="grey",main="")
+hist(savedDraws[,1], 40, xlab = "Log (N)", ylab="Posterior", col="grey",main="")
+hist(savedDraws[,2], 40, xlab = "Environmental coefficient", ylab="Posterior", col="grey",main="")
